494d8a8a78a5350e9ae408417ed3304763cdfa60
s <s@email.com>
2024-12-07 15:37:40 -0400

    last

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/hotspotmobile' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/hotspotmobile' : '',
   images: {
     unoptimized: true
   },
